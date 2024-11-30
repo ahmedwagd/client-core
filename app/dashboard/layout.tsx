5d87108a5f52642f39583761e1312284f9886d4f
Ahmed Wagdy <ahmedwa122@gmail.com>
2024-11-30 16:12:09 +0200

    images

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -92,7 +92,7 @@ export default function DashboardLayout({ children, onLogout }: HeaderProps) {
                   className="relative h-8 w-8 rounded-full"
                 >
                   <Avatar className="h-8 w-8">
-                    <AvatarImage src="/placeholder-user.jpg" alt="User" />
+                    {/* <AvatarImage src="/placeholder-user.jpg" alt="User" /> */}
                     <AvatarFallback>
                       <User className="h-4 w-4" />
                     </AvatarFallback>
